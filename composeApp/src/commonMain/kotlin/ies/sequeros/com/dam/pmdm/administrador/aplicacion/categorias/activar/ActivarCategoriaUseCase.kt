4e42a3a07964b36f40091e5e4df217e3da8a9547
Azael Acin <goodtester24@gmail.com>
2025-12-09 17:50:16 +0100

    Codigo comentado hasta ActivarCategoriaUseCase.

--- composeApp/src/commonMain/kotlin/ies/sequeros/com/dam/pmdm/administrador/aplicacion/categorias/activar/ActivarCategoriaUseCase.kt
+++ composeApp/src/commonMain/kotlin/ies/sequeros/com/dam/pmdm/administrador/aplicacion/categorias/activar/ActivarCategoriaUseCase.kt
@@ -9,15 +9,18 @@ import ies.sequeros.com.dam.pmdm.commons.infraestructura.AlmacenDatos
 class ActivarCategoriaUseCase ( private val repositorio: ICategoriaRepositorio, private val almacenDatos: AlmacenDatos){
 
     suspend fun invoke(command: ActivarCategoriaCommand ): CategoriaDTO {
+        //Busca categoria mediante su Id
         val item: Categoria?=repositorio.getById(command.id)
         if (item==null) {
             throw IllegalArgumentException("El usuario no esta registrado.")
         }
+        //Crea una copia modificada
         var newUser= item.copy(
             enabled = command.enabled,
         )
+        //Guarfa los cambios en la base de datos
         repositorio.update(newUser)
-        //se devuelve con el path correcto
+        //Se devuelve con el path correcto
         return newUser.toDTO(almacenDatos.getAppDataDir()+"/categorias/")
     }
 }
